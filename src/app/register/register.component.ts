90c0762b184a0a10a65886455497d4e994ea8e93
Iahsea <ngyenchinhhai@gmail.com>
2025-05-06 17:15:01 +0700

    001 two way binding for register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [FooterComponent],
+  imports: [FooterComponent, FormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
 
+  constructor() {
+    this.phone = "";
+    this.password = "";
+    this.retypePassword = "";
+    this.fullName = "";
+    this.address = "";
+    this.isAccepted = false;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
+  }
+
+  register() {
+    const message = `${this.phone} + ${this.password} + ${this.retypePassword}
+                      ${this.fullName} + ${this.address} + ${this.isAccepted} + ${this.dateOfBirth}`
+    alert(message);
+  }
 }
