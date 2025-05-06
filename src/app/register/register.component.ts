5e7744f2527c4efead8aca6a64a3768957acb0e2
Iahsea <ngyenchinhhai@gmail.com>
2025-05-06 22:24:12 +0700

    chap 7 002 validate angular form with ngIf and some way to validate

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FooterComponent, FormsModule],
+  standalone: true,
+  imports: [FooterComponent, FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
+
   phone: string;
   password: string;
   retypePassword: string;
@@ -16,6 +20,9 @@ export class RegisterComponent {
   address: string;
   isAccepted: boolean;
   dateOfBirth: Date;
+  phoneTouched = false;
+  passwordTouched = false;
+
 
   constructor() {
     this.phone = "";
@@ -37,4 +44,35 @@ export class RegisterComponent {
                       ${this.fullName} + ${this.address} + ${this.isAccepted} + ${this.dateOfBirth}`
     alert(message);
   }
+
+  //how to check password match ?
+  checkPasswordsMatch() {
+    if (this.password !== this.retypePassword) {
+      this.registerForm.form.controls['retypePassword'].setErrors({ 'passwordMismatch': true });
+    } else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      // Điều chỉnh tuổi nếu chưa tới sinh nhật trong năm nay
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({ invalidAge: true });
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
+  }
+
+
 }
