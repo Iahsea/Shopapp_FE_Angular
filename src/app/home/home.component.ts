2e81f024f2436cdb07fb0fb4bb908806e9a0b0b9
Iahsea <ngyenchinhhai@gmail.com>
2025-05-10 18:41:27 +0700

    chapter 8 001 call api get all products and paging in angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { DetailProductComponent } from '../components/detail-product/detail-product.component';
@@ -6,13 +6,84 @@ import { OrderComponent } from '../components/order/order.component';
 import { OrderConfirmComponent } from '../components/order-confirm/order-confirm.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
+import { error } from 'console';
+import { response } from 'express';
+import { environment } from '../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, DetailProductComponent, OrderComponent, OrderConfirmComponent, LoginComponent, RegisterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  currentPage: number = 0;
+  itemsPerPage: number = 10;
+  page: number[] = [];
+  totalPages: number = 0;
+  visiblePages: number[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.getProduct(this.currentPage, this.itemsPerPage)
+  }
+
+  getProduct(page: number, limit: number) {
+    this.productService.getProducts(page, limit).subscribe(
+      {
+        next: (response: any) => {
+          debugger
+          response.products.forEach((product: Product) => {
+            product.url = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
+          });
+          this.products = response.products;
+          this.totalPages = response.totalPages;
+          this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
+        },
+        complete: () => {
+          debugger
+
+        },
+        error: (error: any) => {
+          debugger
+          console.log('Error fetching products:', error);
+        }
+      }
+    )
+  }
+
+  onPageChange(page: number) {
+    debugger
+    this.currentPage = page;
+    this.getProduct(this.currentPage, this.itemsPerPage);
+  }
+
+  generateVisiblePageArray(currentPage: number, totalPages: number): number[] {
+    const maxVisiblePages = 5;
+    const halfVisiblePages = Math.floor(maxVisiblePages / 2);
+
+    let startPage = Math.max(currentPage - halfVisiblePages, 0);
+    let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages - 1);
+
+    if (endPage - startPage + 1 < maxVisiblePages) {
+      startPage = Math.max(endPage - maxVisiblePages + 1, 0);
+    }
+
+    return new Array(endPage - startPage + 1).fill(0).map((_, index) => startPage + index);
+  }
 
 }
